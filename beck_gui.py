b83095a45e008b9e5afa32071097b3a5e037d044
ecolijah <ecolijah@gmail.com>
2023-06-22 17:59:44 -0500

    rolling memory log added, chatbot is now coherent within the scope of conversation

--- beck_gui.py
+++ beck_gui.py
@@ -12,28 +12,95 @@ load_dotenv()
 # constants
 green_color = "#00EE00"
 red_color = "#EE6363"
-i = 0 # finding voice
-
+voiceid ="HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
+# make changes so that while app is running we use variable memory, and before it closes it writes latest copy to messages log.
+rolling_messages = []
+rolling_messages_count = 10
+messages_filepath = 'rolling_messages.txt'
+proompt_filepath = 'prompt.txt'
 
 # function that sends message to API and returns text response.
 ####### need to add memory of past conversations somehow
-def chat(text):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are an assistant named Beck, who is helpful but not enthusiastic. You are concise with your answers and do not say more than you need to."},
-            {"role": "user", "content": text}
-        ]
-    )
-    return response.choices[0].message.content.strip()
+# for this version we will go with a rolling memory of the past 10 or so chats back and forth. 
+def append_to_rolling_messages(user_text, ai_text):
+        # append
+    global rolling_messages
+    tmpstr = "USER: " + user_text + "\n" + "BECK: " + ai_text
+    tmp_rolling_messages = []
+    if len(rolling_messages) < rolling_messages_count:
+        # Read the file and convert its contents into a list
+        with open(messages_filepath, 'r') as file:
+            lines = file.readlines()
+        tmp_rolling_messages = [line.strip() for line in lines if line.strip()]
+        tmp_rolling_messages.append(tmpstr)
+        # updates global list
+        rolling_messages = tmp_rolling_messages
+        write_to_file(messages_filepath, tmp_rolling_messages)  
 
+    else:
+        # delete oldest append newest
+        # Read the file and retrieve the last 9 lines
+        with open(messages_filepath, 'r') as file:
+            lines = file.readlines()[-rolling_messages_count-1:]
+        tmp_rolling_messages = [line.strip() for line in lines if line.strip()]
+        # appends newest convo
+        tmp_rolling_messages.append(tmpstr)
+        # updates global list
+        rolling_messages = tmp_rolling_messages 
+        write_to_file(messages_filepath, tmp_rolling_messages)  
+
+    return
+
+def write_to_file(filename, lines):
+    # Clear the file
+    with open(filename, 'w') as file:
+        file.write('')
+
+    # Write the lines to the file
+    with open(filename, 'a') as file:
+        for line in lines:
+            file.write(line + '\n')
+
+def chat(prompt_):
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt_,
+        max_tokens=1000,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+    #append to rolling messages
+    return response.choices[0].text.strip()
+
+def load_conversation(file_path):
+    # Read the file and return its content as a string
+    with open(file_path, 'r') as file:
+        file_content = file.read()
+    return file_content
+
+def generate_prompt(user_recent_text):
+    # TODO: add previous conversation, and most recent user query to prompt
+    conversation = load_conversation(messages_filepath)
+    prompt = open_file_edit_contents(proompt_filepath, "<<CONVERSATION>>", conversation, "<<USER_RECENT_QUERY>>", user_recent_text)
+    print(prompt + '\n')
+    return prompt
+def open_file_edit_contents(file_path, old_str_1, new_str_1, old_str_2, new_str_2):
+    # Read the file
+    with open(file_path, 'r') as file:
+        content = file.read()
+        content = content.replace(old_str_1, new_str_1)
+        content = content.replace(old_str_2, new_str_2)
+    
+    # Return the replace_and_save method as a dot method
+    return content
 
 # function that turns text to speech and speaks it.
 def text_to_speech(ai_text):
     engine = pyttsx3.init()
-    engine.setProperty("voice", "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0")
+    engine.setProperty("voice", voiceid)
 
-    engine.setProperty("rate", 200)  # You can adjust the speech rate here
+    engine.setProperty("rate", 190)  # You can adjust the speech rate here
     engine.say(ai_text)
     engine.runAndWait()
 
@@ -83,12 +150,16 @@ class Beck:
                 text = self.recognizer.recognize_google(audio)
                 print("User:", text)
                 # Send call to OpenAI API key.
-                ai_response = chat(text)
-                print("Beck:", ai_response)
+                #generate prompt
+                prompt = generate_prompt(text)
+                ai_response = chat(prompt)
+                print("BECK:", ai_response)
 
                 # Convert the AI response to speech.
+                ## TODO: add to rolling messages
+                
                 text_to_speech(ai_response)
-
+                append_to_rolling_messages(text, ai_response)
                 # Terminate the application
                 if text in ["bye-bye", "bye", "goodbye"]:
                     self.root.destroy()
